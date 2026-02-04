d3cebc80cdb907a36a67972448f4db2de74d4eb2
Trishit Hazra <trishitgithub@gmail.com>
2026-02-04 12:18:15 +0530

    Added route for student admission

---
+++ Backend/routes/admissionRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { auth } from "../middleware/authMiddleware.js";
+import {
+    createAdmissionApplication,
+    getMyApplication,
+} from "../controllers/AdmissionApplicationController.js";
+
+const router = express.Router();
+
+// Only students can create or view their own applications
+router.post("/apply", auth(["student"]), createAdmissionApplication);
+router.get("/me", auth(["student"]), getMyApplication);
+
+export default router;
